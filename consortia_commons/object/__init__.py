4b27f6b2ca589b684f576187f64eddff0d0836d3
Lisa-Ann B <lisabruney@pitt.edu>
2023-03-20 16:00:11 -0400

    Add ability to add value formatter

--- consortia_commons/object/__init__.py
+++ consortia_commons/object/__init__.py
@@ -3,17 +3,17 @@ from typing import Union
 from consortia_commons.string import to_pascal_case
 
 
-def build_enum_class(class_name: str, data: Union[list, dict], key: str = None):
+def build_enum_class(class_name: str, data: Union[list, dict], key: str = None, val_callback=None):
     _enums = {}
     try:
         if type(data) is list:
             for item in data:
                 val = item.get(key)
-                _enums[to_pascal_case(val)] = val
+                _enums[to_pascal_case(val)] = val if val_callback is None else val_callback(val)
         else:
             if key is not None:
                 val = data.get(key)
-                _enums[to_pascal_case(val)] = val
+                _enums[to_pascal_case(val)] = val if val_callback is None else val_callback(val)
             else:
                 _enums = data
         return enum.Enum(class_name, _enums)
