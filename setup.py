536b90d3dc9ae41f8a16b20b61be612f81057a5f
maxsibilla <maxsibilla@icloud.com>
2023-03-21 13:20:20 -0400

    Updating version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="atlas-consortia-commons",
-    version="0.0.1",
+    version="1.0.0",
     author="Atlas Consortia",
     author_email="api-developers@hubmapconsortium.org",
     description="The common code supporting the web services in the consortia.",
