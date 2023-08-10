2774b5d8db0757ffb7bcd9d909f7ddad3768e085
maxsibilla <maxsibilla@icloud.com>
2023-08-10 12:42:18 -0400

    Bumping version

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name="atlas-consortia-commons",
     # Test PyPi version
-    # version="1.0.4"
-    version="1.0.3",
+    version="1.0.4",
     author="Atlas Consortia",
     author_email="api-developers@hubmapconsortium.org",
     description="The common code supporting the web services in the consortia.",
