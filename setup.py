eae10a23162afd91b0500188992b9d81fc929e24
Lisa-Ann B <115558393+libpitt@users.noreply.github.com>
2026-06-22 14:13:26 -0400

    Update test version

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ with open("README.md", "r") as fh:
 setup(
     name="atlas_consortia_commons",
     # Test PyPi version
-    # version="1.1.3",
+    # version="1.1.4",
     # Prod PyPi version
-    version="1.1.5",
+    version="1.1.3",
     author="Atlas Consortia",
     author_email="api-developers@hubmapconsortium.org",
     description="The common code supporting the web services in the consortia.",
